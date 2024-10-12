3a692f86bd24f88b088a7f315cb8c8e951e473c3
Jorge Diz Pico <xurxo@xurxodiz.eu>
2024-10-12 03:34:08 +0200

    Style "blog" archive same as any other archive

--- content/archive-header.php
+++ content/archive-header.php
@@ -1,17 +1,6 @@
 <?php
 
-// Don't output on bbPress (Forum pages count as archives)
-if ( function_exists( 'is_bbpress' ) ) {
-	if ( is_bbpress() ) {
-		return;
-	} 
-}
-
-if ( is_home() ) {
-	echo '<h1 class="screen-reader-text">' . esc_html( get_bloginfo("name") ) . ' ' . _x('Posts', 'noun: Site Title\' Posts', 'author') . '</h1>';
-}
-
-if ( ! is_archive() ) {
+if ( ! is_archive() || ! is_home() ) {
 	return;
 }
 
@@ -23,13 +12,15 @@ if ( is_tag() ) {
 	$icon_class = 'user';
 } elseif ( is_date() ) {
 	$icon_class = 'calendar';
+} elseif ( is_home() ) {
+	$icon_class = 'box-archive';
 }
 ?>
 
 <div class='archive-header'>
 	<i class="fas fa-<?php echo $icon_class; ?>" aria-hidden="true"></i>
 	<h1>
-		<?php the_archive_title(); ?>
+		<?php is_archive() ?? the_archive_title() : echo 'Arquivo'; ?>
 	</h1>
-	<?php the_archive_description(); ?>
+	<?php is_archive() ?? the_archive_description() : echo "Todas as entradas existentes."; ?>
 </div>
